49cf2087f960ce72c5e84367d1077d3fb41a7a93
ramosws03 <107620008+ramosws03@users.noreply.github.com>
2023-10-21 12:22:11 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Halaman Web</title>
+</head>
+<body>
+    <h1>Selamat Datang di Halaman Web Sederhana</h1>
+    <p>Ini adalah contoh dari halaman HTML.</p>
+</body>
+</html>
